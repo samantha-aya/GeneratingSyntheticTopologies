9b60688703a9d1b9cb16c7a7d9044916de39929e
SanjanaKunkolienkar <99448403+SanjanaKunkolienkar@users.noreply.github.com>
2023-11-03 08:21:47 -0500

    modified for 37 bus case

--- GenerateCyberPowerTopology.py
+++ GenerateCyberPowerTopology.py
@@ -5,13 +5,13 @@ import pandas as pd
 cwd = os.getcwd()
 
 #get input substation data from excel
-substation_data = pd.read_excel(os.path.join(cwd, 'Input\\', 'McAllenSubstationCoordinates.xlsx'))
+substation_data = pd.read_excel(os.path.join(cwd, 'Input\\', '37BusCaseSubstations.xlsx'))
 
 #Convert DataFrame to dictionary with substation number as key and lat,lon as values
-location_dict = substation_data.set_index('Substation Number')[['Substation Latitude', 'Substation Longitude']].T.apply(tuple).to_dict()
+location_dict = substation_data.set_index('Sub Num')[['Latitude', 'Longitude']].T.apply(tuple).to_dict()
 
 #Calling functions from utility_model.py
-substationNumbers = substation_data['Substation Number'].tolist()
+substationNumbers = substation_data['Sub Num'].tolist()
 utilityNumber = [2]
 generateSubstationLevel(substationNumbers,utilityNumber)
 generateUtilityLevel(substationNumbers, utilityNumber)
